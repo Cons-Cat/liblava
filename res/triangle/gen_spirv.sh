12475a4bd8678fa32bc058c4bbe0b4f6d1c1ebf7
Conscat <wgooch2000@gmail.com>
2021-07-16 14:50:47 -0400

    Improved HLSL triangle in many ways

--- res/triangle/gen_spirv.sh
+++ res/triangle/gen_spirv.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-glslangValidator -V -o fragment.spirv triangle.frag
-glslangValidator -V -o vertex.spirv triangle.vert
+for arg in "$@"
+do
+    case $arg in
+        -glsl)
+            glslangValidator -V -o fragment.spirv glsl/triangle.glsl.frag
+            glslangValidator -V -o vertex.spirv glsl/triangle.glsl.vert
+            shift
+            ;;
+        -hlsl)
+            glslangValidator -e main -V -fhlsl_functionality1 -o fragment.spirv -D hlsl/triangle.hlsl.frag
+            glslangValidator -e main -V -fhlsl_functionality1 -o vertex.spirv -D hlsl/triangle.hlsl.vert
+            shift
+    esac
+done
